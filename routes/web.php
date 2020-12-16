ad484f8d545a420a5c872297797a18fab43a87df
Carlos Ferreira <conta@especializati.com.br>
2020-12-16 14:40:52 -0300

    Test Create New Post

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/create-post', function () {
+    $user = User::first();
+
+    $user->posts()->create([
+        'title' => Str::random(150),
+        'body' => Str::random(400),
+    ]);
+
+    return 'ok';
+});
 
 Route::get('/', function () {
     return view('welcome');
