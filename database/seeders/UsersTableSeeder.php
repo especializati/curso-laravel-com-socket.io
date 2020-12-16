a31653242ab685c30dee67aba9e6ad238e1a566f
Carlos Ferreira <conta@especializati.com.br>
2020-12-16 14:40:41 -0300

    Create Seeder User

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            UsersTableSeeder::class,
-        ]);
+        User::factory(10)->create();
     }
 }
