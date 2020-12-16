04ccbbf88ed633c7f14b00b8fa5e946b8b5af1bc
Carlos Ferreira <conta@especializati.com.br>
2020-12-16 14:40:23 -0300

    Create Migration and Models Posts with relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
