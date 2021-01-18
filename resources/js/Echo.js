8796e1bea98d4937220227ec4f67c39304857c8b
Carlos Ferreira <conta@especializati.com.br>
2021-01-18 19:52:15 -0300

    Notify New Post Created

--- resources/js/Echo.js
+++ resources/js/Echo.js
@@ -1,6 +1,10 @@
+import Vue from 'vue'
+
 
 window.Echo.channel('laravel_database_post-created')
             .listen('PostCreated', (e) => {
                 console.log(e)
                 console.log(e.post)
+
+                Vue.$vToastify.success(`Título do post ${e.post.title}`, 'Novo Post')
             })
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import VueToastify from 'vue-toastify'
+
+Vue.use(VueToastify)
+
+const app = new Vue({
+    el: '#app'
+})
