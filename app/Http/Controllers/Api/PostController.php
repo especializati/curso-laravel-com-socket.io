98c53519d7f6039f8742b5c2182267799a5f0c06
Carlos Ferreira <conta@especializati.com.br>
2021-01-18 20:09:40 -0300

    API Posts Done

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    private $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    public function index()
+    {
+        $posts = $this->post->latest()->get();
+
+        return PostResource::collection($posts);
+    }
+}
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->title,
+            'date' => Carbon::parse($this->created_at)->format('d/m/Y H:i:s')
+        ];
+    }
+}
